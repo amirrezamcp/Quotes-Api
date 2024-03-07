f512dcb70942880c4ded449dfdd7f632caa8ba6b
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-07 22:35:02 +0330

    Create Route

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+header('Content-Type: application/json');
+
+use Controllers\route;
 require_once "autoload.php";
 
 // GET URL and REQUEST Typ
 $requestUrl = parse_url(htmlspecialchars($_SERVER['REQUEST_URI']), PHP_URL_PATH);
 $requestMethod = htmlspecialchars($_SERVER['REQUEST_METHOD']);
 
-echo($requestMethod); 
+$routes = new route();
+$routes->add('/quotes/{id}', 'GET', 'Controllers\quotescontroller', 'index');
+$routes->match($requestUrl, $requestMethod);
