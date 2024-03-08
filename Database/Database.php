fb5247bcf6834eb6afd3e9e14e3fe1bdcd64195b
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-08 20:16:05 +0330

    Created Database

---
+++ Database/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database;
+
+use Traits\SanitizerTrait;
+
+class Database {
+    use SanitizerTrait;
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'ooplogin_dbs';
+    private $connection;
+
+    public function __construct() {
+        $this->connection = new \mysqli($this->host, $this->username, $this->password, $this->database);
+        if ($this->connection->connect_error) {
+            die('Connection Failed: '.$this->connection->connect_error);
+        }
+    }
+    public function __destruct() {
+        $this->connection->close();
+    }
+
+    protected function executeStatement($sql, $params = []) {
+        $stmt = $this->connection->prepare($sql);
+        if (!$stmt) {
+            die("Error in preparing Statement: ".$this->connection->error);
+        }
+        if(!empty($params)) {
+            $types = str_repeat('s', count($params));
+            $stmt->bind_param($types, ...$params);
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+}
