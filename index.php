c8be6f2261991a0d1618d943809f563efb68d31f
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-08 20:19:02 +0330

    Sort file index

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 header('Content-Type: application/json');
-
-use Controllers\route;
 require_once "autoload.php";
+require_once "RouteController.php";
 
 // GET URL and REQUEST Typ
 $requestUrl = parse_url(htmlspecialchars($_SERVER['REQUEST_URI']), PHP_URL_PATH);
 $requestMethod = htmlspecialchars($_SERVER['REQUEST_METHOD']);
 
-$routes = new route();
-$routes->add('/', 'GET', 'Controllers\HomeController', 'home');
-$routes->add('/quotes/{id}', 'GET', 'Controllers\quotesController', 'index');
+
 $routes->match($requestUrl, $requestMethod);
