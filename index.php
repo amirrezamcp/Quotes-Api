9ef0ccfac0eddb4d3bc14739ca17f1da9a59c6b3
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-12 09:53:30 +0330

    debug

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 require_once "autoload.php";
-require_once "RouteController.php";
+require_once "routes.php";
 
 // GET URL and REQUEST Typ
 $requestUrl = parse_url(htmlspecialchars($_SERVER['REQUEST_URI']), PHP_URL_PATH);
