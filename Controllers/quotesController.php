0b7ebf0336606eba7059aec15fddee1b3217031b
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-12 09:51:51 +0330

    class Quotes Controller - query select

--- Controllers/quotesController.php
+++ Controllers/quotesController.php
@@ -1,7 +1,47 @@
 <?php
 namespace Controllers;
-class quotesController{
+use Database\Database;
+class QuotesController extends Database{
+    protected $table = 'quotes';
     public function index($params){
-        echo $params[1];
+        $sql = "SELECT * FROM $this->table";
+        $stmt = $this->executeStatement($sql);
+
+        // $stmt->fetch();
+        $rows = $stmt->get_result();
+        if($rows->num_rows <= 0) {
+            $response = [
+                "status" => "error",
+                "message" => "no record exists in database"
+            ];
+        }else{
+            $data = [];
+            while($row = $rows->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+        http_response_code(200);
+        echo json_encode($data);
+    }
+
+    public function getQuotes($params) {
+        $id = $params['id'];
+        $sql = "SELECT * FROM $this->table WHERE id = ?";
+        $sql_params = [$id];
+        $stmt = $this->executeStatement($sql, $sql_params);
+        $row = $stmt->get_result();
+        $row = $row->fetch_assoc();
+        if(is_null($row)) {
+            $response = [
+                'status' => 'erroe',
+                'message' => 'record not found.'
+            ];
+        }else{
+            $response = $row;
+        }
+        http_response_code(200);
+        echo json_encode($response);
+
+
     }
 }
