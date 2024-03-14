e9c6e9da73e3c5e56277172cc9c3be67fdf6bdf5
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-14 14:47:51 +0330

    POST Quotes routes

--- routes.php
+++ routes.php
@@ -9,3 +9,6 @@ $routes->add('/', 'GET', 'Controllers\HomeController', 'home');
 // GET Quotes routes
 $routes->add('/quotes', 'GET', 'Controllers\QuotesController', 'index');
 $routes->add('/quotes/{id}', 'GET', 'Controllers\QuotesController', 'getQuotes');
+
+// POST Quotes routes
+$routes->add('/quotes', 'POST', 'Controllers\QuotesController', 'stores');
